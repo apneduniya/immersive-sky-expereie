fde87302e685116edb5763e8474b637459c8320f
apneduniya <apneduniya.dontdisturb@gmail.com>
2024-06-30 11:24:56 +0530

    feat: Made home and about page

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,16 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'img-bg-1': "url('/images/bg-1.jpeg')",
+        'img-bg-2': "url('/images/bg-2.jpeg')",
+        'img-bg-3': "url('/images/bg-3.jpeg')",
+        'img-bg-4': "url('/images/bg-4.jpeg')",
+        'img-bg-5': "url('/images/bg-5.jpeg')",
+        'img-bg-6': "url('/images/bg-6.jpeg')",
+        'img-bg-7': "url('/images/bg-7.jpeg')",
+        'img-bg-8': "url('/images/bg-8.jpeg')",
+        'img-bg-9': "url('/images/bg-9.jpeg')",
+        'img-bg-10': "url('/images/bg-10.jpeg')",
       },
     },
   },
